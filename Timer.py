5ebf01c03aef6f8b74fc18c5ab4eb086a4606332
Yarin Michael Azoulay <yarinaz17@gmail.com>
2023-03-03 18:37:09 +0200

    Revert "First commit"
    This reverts commit 4b30617af61c2ec6b9259b6552ac326162f5d3ad.

--- Timer.py
+++
@@ -1,7 +0,0 @@
-import time
-
-def countdown(sec : int):
-    while sec > 0:
-        print(f"Retrying in {sec} seconds")
-        time.sleep(1)
-        sec -= 1
--- appointment_bot.py
+++
@@ -1,9 +0,0 @@
-
-from AppointmentClient import AppointmentClient
-
-def main():
-   appointment_client = AppointmentClient()
-   appointment_client.run()
-   
-if __name__ == '__main__':
-    main()
